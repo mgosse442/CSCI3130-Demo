e5291f673098b9c754992db6d8421a8d05c84ad2
Megan <megangosse@gmail.com>
2018-05-19 18:30:20 -0300

    Added text field and button

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        EditText editText = findViewById(R.id.editText);
+        String message = editText.getText().toString();
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
     }
 }
